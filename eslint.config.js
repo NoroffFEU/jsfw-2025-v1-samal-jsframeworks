446074f1b959a5dd2ee4f1c96e742f2209b3cab9
Samal <56509582+FransoArbela@users.noreply.github.com>
2025-10-16 16:49:38 +0200

    chore: update dependencies and configure Vite with SWC plugin
    - Added @vitejs/plugin-react-swc to package.json for improved React support
    - Included eslint-plugin-react-dom and eslint-plugin-react-x for enhanced linting
    - Updated vite.config.ts to use @vitejs/plugin-react-swc instead of @vitejs/plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,40 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import reactPlugin from 'eslint-plugin-react'
+import reactX from 'eslint-plugin-react-x'
+import reactDOM from 'eslint-plugin-react-dom'
 import hooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from 'eslint/config'
 
-export default tseslint.config(
+export default defineConfig([
+  { ignores: ['dist', 'coverage', '.vite'] },
+
   js.configs.recommended,
-  ...tseslint.configs.recommended,
+
   {
     files: ['**/*.{ts,tsx}'],
-    plugins: { react: reactPlugin, 'react-hooks': hooks, 'react-refresh': reactRefresh },
+    extends: [
+      tseslint.configs.recommendedTypeChecked,
+      reactX.configs.recommended,      
+      reactDOM.configs.recommended,   
+    ],
+    plugins: {
+      'react-x': reactX,
+      'react-dom': reactDOM,
+      'react-hooks': hooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
     rules: {
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
       'react-refresh/only-export-components': 'warn',
       'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn'
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-x/no-class-component': 'warn',
     },
-    settings: { react: { version: 'detect' } }
-  }
-)
+  },
+])
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 import svgr from "vite-plugin-svgr";
+import react from '@vitejs/plugin-react-swc'
+
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
