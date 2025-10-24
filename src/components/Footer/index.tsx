79334c906d576e4baee3f714054194f695fcb4a9
Samal <56509582+FransoArbela@users.noreply.github.com>
2025-10-24 12:46:52 +0200

    refactor: Standardize code formatting across App, Cart, Footer, and Home components for improved readability

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-
 const Footer = () => {
-  return (
-    <footer className="py-4 text-center text-gray-600 bg-white/90 backdrop-blur shadow mt-4">
-      &copy; {new Date().getFullYear()} Zeebra. All rights reserved.
-    </footer>
-  );
-};
+	return (
+		<footer className="py-4 text-center text-gray-600 bg-white/90 backdrop-blur shadow mt-4">
+			&copy; {new Date().getFullYear()} Zeebra. All rights reserved.
+		</footer>
+	)
+}
 
-export default Footer;
+export default Footer
